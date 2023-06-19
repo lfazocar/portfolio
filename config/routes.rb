d824e688fcfe8a5b9e8c1fa1799718dddc0a9dde
Felipe <lfazocar@users.noreply.github.com>
2023-06-19 12:50:57 -0400

    added projects and contact skeleton, fixed some syntax and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'pages/home'
-  get 'pages/projects'
-  get 'pages/contact'
+  get '/home', to: 'pages#home'
+  get '/projects', to: 'pages#projects'
+  get '/contact', to: 'pages#contact'
 
   # Defines the root path route ("/")
   # root "articles#index"
