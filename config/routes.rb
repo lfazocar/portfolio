fbec5f3a776db06b0a88b19d4aa97e3646811533
Felipe <lfazocar@users.noreply.github.com>
2023-06-19 11:58:39 -0400

    added shared navbar, routes for projects and contact and some progress in home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'pages/home'
+  get 'pages/projects'
+  get 'pages/contact'
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'pages#home'
 end
